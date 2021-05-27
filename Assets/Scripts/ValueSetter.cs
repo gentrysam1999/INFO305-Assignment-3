2a55abd318653618950379e7888c47add5eb0b61
Sam Gentry <gensa844@student.otago.ac.nz>
2021-05-27 15:15:42 +1200

    adding sliders to set height and weight

---
+++ Assets/Scripts/ValueSetter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class ValueSetter : MonoBehaviour
+{
+    public GameObject heightText;
+    public GameObject heightSlider;
+    public int height = 150;
+    public int weight = 50;
+
+    public void SetHeight()
+    {
+        float sliderValue = heightSlider.GetComponent<PinchSlider>().SliderValue;
+
+    }
+
+    public void SetWeight()
+    {
+        
+    }
+}
